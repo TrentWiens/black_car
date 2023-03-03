743b7a8efb3a76ef4eed4805fe0ac71df87807d9
TrentWiens <trent.wiens@huskers.unl.edu>
2023-03-03 11:27:08 -0800

    safety.py

--- scripts/trentwiens_safety.py
+++ scripts/trentwiens_safety.py
@@ -12,15 +12,16 @@ class Safety(object):
 
 	def __init__(self):
 	
-		self.speed = 0
-		self.odom = rospy.Subscriber("odom", Odometry, self.odom_callback)
+		self.speed = 0.5
+#		self.odom = rospy.Subscriber("odom", Odometry, self.odom_callback)
 		self.scan = rospy.Subscriber("scan", LaserScan, self.scan_callback)
-		self.brakePub = rospy.Publisher("brake", AckermannDriveStamped, queue_size = 10)
-		self.brakeBoolPub = rospy.Publisher("brake_bool", Bool, queue_size = 10)
+		self.brakePub = rospy.Publisher("/vesc/high_level/ackermann_cmd_mux/input/nav_0", AckermannDriveStamped, queue_size = 10)
+#		self.brakePub = rospy.Publisher("brake", AckermannDriveStamped, queue_size = 10)
+#		self.brakeBoolPub = rospy.Publisher("brake_bool", Bool, queue_size = 10)
 		
-	def odom_callback(self, odom_msg):
+#	def odom_callback(self, odom_msg):
 
-		self.speed = odom_msg.twist.twist.linear.x
+#		self.speed = odom_msg.twist.twist.linear.x
 		
 	def scan_callback(self, scan_msg):
 	
@@ -35,19 +36,19 @@ class Safety(object):
 		index = ranges.index(smallRange)
 		angle = minAngle + incAngle * index
 		rDot = np.cos(angle) * speed
-		if rDot > 0 and smallRange/rDot < smal7lestTTC: 
+		if rDot > 0 and smallRange/rDot < smallestTTC: 
 			smallestTTC = smallRange/rDot
 			
 		if smallestTTC < minTTC: 
-			brake_bool = True
-			self.brakeBoolPub.publish(brake_bool)
+#			brake_bool = True
+#			self.brakeBoolPub.publish(brake_bool)
 			ackMsg = AckermannDriveStamped()
 			ackMsg.header = scan_msg.header
 			ackMsg.drive.speed = 0
 			self.brakePub.publish(ackMsg)
 		else: 
-			brake_bool = False
-			self.brakeBoolPub.publish(brake_bool)
+#			brake_bool = False
+#			self.brakeBoolPub.publish(brake_bool)
 				
 def main():
 	rospy.init_node('safety_node')
